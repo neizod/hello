a973745c1c9f02d527371dd879f96b9d865dd5cc
Nattawut Phetmak <neizod@gmail.com>
2012-07-29 00:38:23 +0700

    add option -s as sorted name

--- hello.py
+++ hello.py
@@ -5,6 +5,10 @@ names = sys.argv[1:]
 if not names:
     print("Hello, world!")
 
+if '-s' in names:
+    names.remove('-s')
+    names.sort()
+
 for name in names:
     print("Hi {}.".format(name))
 
